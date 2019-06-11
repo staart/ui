323d8e36a855e2772d018040c221f7b76c436131
Anand Chowdhary <mail@anandchowdhary.com>
2019-06-11 15:31:22 +0200

    :recycle: Move Staart domain to Oswald Labs

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -46,7 +46,7 @@ const config: NuxtConfiguration = {
   axios: {
     host:
       process.env.NODE_ENV === "production"
-        ? "staart.caprover.o15y.com"
+        ? "staart.caprover.oswaldlabs.com"
         : "localhost",
     https: process.env.NODE_ENV === "production",
     port: process.env.NODE_ENV === "production" ? 443 : 7007
