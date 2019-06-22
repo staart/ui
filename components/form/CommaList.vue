b170b304cb9f2fa630ae3ccba945da554614f339
Anand Chowdhary <mail@anandchowdhary.com>
2019-06-22 22:49:59 +0200

    :construction: Comma separated data component

---
+++ components/form/CommaList.vue
@@ -0,0 +1,132 @@
+<template>
+  <div class="form-group">
+    <label :for="labelId">{{ label }}</label>
+    <table class="table">
+      <tbody>
+        <tr v-for="(item, index) in list" :key="`p_${index}`">
+          <td>
+            <input
+              :id="labelId"
+              :value="item"
+              :placeholder="placeholder"
+              :autocomplete="autocomplete"
+              :required="required"
+              v-bind="$attrs"
+              @input="val => input(index, val)"
+            />
+          </td>
+          <td class="text text--align-right">
+            <button
+              aria-label="Remove"
+              data-balloon-pos="up"
+              class="button button--type-icon button--color-danger"
+              type="button"
+              @click.prevent="remove(index)"
+            >
+              <font-awesome-icon
+                class="icon icon--color-danger"
+                icon="times"
+                fixed-width
+              />
+            </button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+    <button class="button button--type-icon" type="button" @click.prevent="add">
+      <font-awesome-icon class="icon icon--mr-1" icon="plus" fixed-width />
+      <span>Add another</span>
+    </button>
+    <div
+      v-if="help"
+      class="text text--size-small text--color-light text--lh-1 section section--mt-05"
+    >
+      {{ help }}
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from "vue-property-decorator";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faTimes, faPlus } from "@fortawesome/free-solid-svg-icons";
+library.add(faTimes, faPlus);
+
+@Component({
+  components: {
+    FontAwesomeIcon
+  }
+})
+export default class CommaList extends Vue {
+  @Prop() value;
+  @Prop() label;
+  @Prop() required;
+  @Prop() placeholder;
+  @Prop() help;
+  @Prop() autocomplete;
+  labelId = "";
+  list = [""];
+
+  get commaList() {
+    return this.list.filter(item => !!item).join(",");
+  }
+
+  private created() {
+    this.labelId = Math.random()
+      .toString(36)
+      .substring(7);
+  }
+
+  private add() {
+    this.list.push("");
+  }
+
+  private remove(index) {
+    this.list.splice(index, 1);
+  }
+
+  private input(index, val) {
+    this.list[index] = val.target.value;
+    this.$emit("input", this.commaList);
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.form-group {
+  margin-bottom: 1rem;
+  label {
+    display: block;
+    margin-bottom: 0.5rem;
+  }
+  table {
+    margin-bottom: 1rem;
+    td,
+    th {
+      padding: 0;
+      padding-top: 0.5rem;
+      &:last-child {
+        width: 7%;
+      }
+    }
+    tr:nth-child(even) {
+      background-color: transparent;
+    }
+  }
+  input {
+    font: inherit;
+    padding: 0.75rem;
+    display: block;
+    width: 100%;
+    box-sizing: border-box;
+    border-radius: 0.2rem;
+    border: 1px solid rgba(121, 82, 179, 0.25);
+    background-color: #fff;
+    &:focus {
+      outline: none;
+      box-shadow: 0 0 0 3px rgba(121, 82, 179, 0.25);
+    }
+  }
+}
+</style>
---
+++ pages/manage/_team/api-keys/_key.vue
@@ -0,0 +1,188 @@
+<template>
+  <main>
+    <Loading v-if="loading" :message="loading" />
+    <div v-else>
+      <div class="row">
+        <h1>API keys</h1>
+        <div class="text text--align-right">
+          <button
+            aria-label="Refresh"
+            data-balloon-pos="down"
+            class="button button--type-icon"
+            @click="load"
+          >
+            <font-awesome-icon class="icon" icon="sync" fixed-width />
+          </button>
+        </div>
+      </div>
+      <form
+        v-if="apiKey"
+        v-meta-ctrl-enter="updateApiKey"
+        @submit.prevent="updateApiKey"
+      >
+        <CommaList
+          label="IP restrictions"
+          :value="apiKey.ipRestrictions"
+          placeholder="Enter an IP address or CIDR, e.g., 192.168.1.1/42"
+          @input="
+            val => {
+              apiKey.ipRestrictions = val;
+              $forceUpdate();
+            }
+          "
+        />
+        {{ apiKey.ipRestrictions }}
+        <CommaList
+          label="Referrer restrictions"
+          :value="apiKey.referrerRestrictions"
+          placeholder="Enter a referrer URL, e.g., http*://*.example.com"
+          @input="val => (apiKey.referrerRestrictions = val)"
+        />
+        <p><strong>Access</strong></p>
+        <div class="fake-radio-container">
+          <label>
+            <input v-model="apiKey.access" type="radio" :value="0" required />
+            <span class="fake-radio" role="radio" tabindex="0" />
+            <span class="name">Read-only access</span>
+          </label>
+        </div>
+        <div class="fake-radio-container">
+          <label>
+            <input v-model="apiKey.access" type="radio" :value="1" required />
+            <span class="fake-radio" role="radio" tabindex="0" />
+            <span class="name">Full access</span>
+          </label>
+        </div>
+        <button class="button">Update API key</button>
+      </form>
+    </div>
+    <transition name="modal">
+      <Confirm v-if="showDelete" :on-close="() => (showDelete = false)">
+        <h2>Are you sure you want to delete this API key?</h2>
+        <p>
+          Deleting an API key is not reversible, and you'll need to update any
+          apps using this key.
+        </p>
+        <button
+          class="button button--color-danger-cta"
+          @click="deleteApiKey(showDelete.apiKey)"
+        >
+          Yes, delete API key
+        </button>
+        <button type="button" class="button" @click="showDelete = false">
+          No, don't delete
+        </button>
+      </Confirm>
+    </transition>
+  </main>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Watch } from "vue-property-decorator";
+import { mapGetters } from "vuex";
+import Loading from "@/components/Loading.vue";
+import Confirm from "@/components/Confirm.vue";
+import TimeAgo from "@/components/TimeAgo.vue";
+import LargeMessage from "@/components/LargeMessage.vue";
+import CommaList from "@/components/form/CommaList.vue";
+import Checkbox from "@/components/form/Checkbox.vue";
+import Select from "@/components/form/Select.vue";
+import { getAllCountries } from "countries-and-timezones";
+import { User } from "@/types/auth";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faPencilAlt,
+  faArrowDown,
+  faSync,
+  faTrash,
+  faEye,
+  faEyeSlash
+} from "@fortawesome/free-solid-svg-icons";
+import { ApiKeys, emptyPagination, ApiKey } from "../../../../types/manage";
+library.add(faPencilAlt, faArrowDown, faSync, faTrash, faEye, faEyeSlash);
+
+@Component({
+  components: {
+    Loading,
+    Confirm,
+    TimeAgo,
+    CommaList,
+    FontAwesomeIcon,
+    Select,
+    LargeMessage,
+    Checkbox
+  },
+  middleware: "auth"
+})
+export default class ManageSettings extends Vue {
+  apiKeys: ApiKeys = emptyPagination;
+  showDelete = false;
+  loading = "";
+  apiKey: ApiKey | null = null;
+
+  private created() {
+    this.apiKeys = {
+      ...this.$store.getters["manage/apiKeys"](this.$route.params.team)
+    };
+  }
+
+  private load() {
+    this.loading = "Loading your API keys";
+    this.$store
+      .dispatch("manage/getApiKey", {
+        team: this.$route.params.team,
+        id: this.$route.params.key
+      })
+      .then(apiKey => {
+        this.apiKey = { ...apiKey };
+      })
+      .catch(error => {
+        throw new Error(error);
+      })
+      .finally(() => (this.loading = ""));
+  }
+
+  private mounted() {
+    this.load();
+  }
+
+  private updateApiKey() {
+    this.loading = "Creating your API key";
+    this.$store
+      .dispatch("manage/updateApiKey", {
+        team: this.$route.params.team,
+        id: this.$route.params.key,
+        ...this.apiKey
+      })
+      .then(apiKeys => {
+        this.apiKeys = { ...apiKeys };
+      })
+      .catch(error => {
+        throw new Error(error);
+      })
+      .finally(() => {
+        this.loading = "";
+      });
+  }
+
+  private deleteApiKey(key: string) {
+    this.showDelete = false;
+    this.loading = "Deleting your API key";
+    this.$store
+      .dispatch("manage/deleteApiKey", {
+        team: this.$route.params.team,
+        id: key
+      })
+      .then(apiKeys => {
+        this.apiKeys = { ...apiKeys };
+      })
+      .catch(error => {
+        throw new Error(error);
+      })
+      .finally(() => (this.loading = ""));
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
