e751f45e328188e2930259b35f1ffe3ab14dabe6
Anand Chowdhary <mail@anandchowdhary.com>
2019-09-04 13:38:55 +0530

    :recycle: Change dist to public

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,9 @@ import { Configuration } from "@nuxt/types";
 
 const config: Configuration = {
   mode: "universal",
+  generate: {
+    dir: "public"
+  },
   head: {
     title: "Staart UI",
     meta: [
