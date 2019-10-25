a8ec58ebb3b48931734cafa08fe9bd6cef2a0144
Anand Chowdhary <mail@anandchowdhary.com>
2019-10-25 15:53:22 -0700

    :white_check_mark: Add tests

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,3 +1,30 @@
-test("should be a string", () => {
-  expect(typeof "random()").toBe("string");
+import { exec } from "shelljs";
+import { join } from "path";
+import { readFileSync } from "fs";
+import { JSDOM } from "jsdom";
+
+jest.setTimeout(60000);
+const publicDir = join(__dirname, "..", "public");
+
+beforeAll(async () => {
+  exec("yarn build");
+});
+
+test("index.html is created", () => {
+  readFileSync(join(publicDir, "index.html"));
+});
+
+test("index.html has a title", () => {
+  readFileSync(join(publicDir, "index.html")).toString().includes("<title>Staart UI");
+});
+
+test("pwa is build", () => {
+  readFileSync(join(publicDir, "site.webmanifest"));
+});
+
+test("login form has a password input", () => {
+  const html = readFileSync(join(publicDir, "auth", "login", "index.html")).toString();
+  const { window } = new JSDOM(html).window;
+  const element = window.document.querySelector("form input[type='password']");
+  expect(element).toBeTruthy();
 });
