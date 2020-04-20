5517547e14a253b47a87308bfbf608c426bb7604
Anand Chowdhary <mail@anandchowdhary.com>
2020-04-20 19:18:07 +0530

    :sparkles: Add nav, footer component

---
+++ components/layout/Footer.vue
@@ -0,0 +1,12 @@
+<template>
+  <footer class="footer">
+    <div class="content has-text-centered">
+      <p>
+        <strong>Staart</strong> by
+        <a href="https://anandchowdhary.com">Anand Chowdhary</a> from
+        <a href="https://o15y.com">O15Y</a>. The source code is licensed
+        <a href="http://opensource.org/licenses/mit-license.php">MIT</a>.
+      </p>
+    </div>
+  </footer>
+</template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -2,27 +2,18 @@
   <div>
     <Navbar />
     <main><nuxt /></main>
-    <footer class="footer">
-      <div class="content has-text-centered">
-        <p>
-          <strong>Staart</strong> by
-          <a href="https://anandchowdhary.com">Anand Chowdhary</a> from
-          <a href="https://o15y.com">O15Y</a>. The source code is licensed
-          <a href="http://opensource.org/licenses/mit-license.php">MIT</a>.
-        </p>
-      </div>
-    </footer>
+    <Footer />
   </div>
 </template>
 
 <script lang="ts">
-import { Vue, Component } from "vue-class-component";
+import Vue from "vue";
+import Component from "vue-class-component";
 import Navbar from "@/components/layout/Navbar.vue";
+import Footer from "@/components/layout/Footer.vue";
 
 @Component({
-  components: { Navbar }
+  components: { Navbar, Footer }
 })
-export default class Navbar extends Vue {
-  message: string = "Hello!";
-}
+export default class Default extends Vue {}
 </script>
