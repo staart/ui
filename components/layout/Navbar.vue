e4772ee2b654dc0d02225f22ebbc43115a80dd1b
Anand Chowdhary <mail@anandchowdhary.com>
2020-04-20 19:15:46 +0530

    :heavy_plus_sign: Add vue-class-component

---
+++ components/layout/Navbar.vue
@@ -0,0 +1,43 @@
+<template>
+  <b-navbar wrapper-class="container" :transparent="true">
+    <template slot="brand">
+      <b-navbar-item tag="router-link" :to="{ path: '/' }">
+        <img
+          src="https://raw.githubusercontent.com/staart/api/master/assets/logo.png"
+          alt="Staart"
+        />
+      </b-navbar-item>
+    </template>
+    <template slot="start">
+      <b-navbar-item href="#">
+        Features
+      </b-navbar-item>
+      <b-navbar-item href="#">
+        Use Cases
+      </b-navbar-item>
+      <b-navbar-item href="#">
+        Pricing
+      </b-navbar-item>
+      <b-navbar-dropdown label="Resources" hoverable boxed>
+        <b-navbar-item href="#">
+          Documentation
+        </b-navbar-item>
+        <b-navbar-item href="#">
+          Accessibility
+        </b-navbar-item>
+      </b-navbar-dropdown>
+    </template>
+    <template slot="end">
+      <b-navbar-item tag="div">
+        <div class="buttons">
+          <nuxt-link to="/auth/register" class="button is-primary">
+            <strong>Get started for free</strong>
+          </nuxt-link>
+          <nuxt-link to="/auth/login" class="button is-light">
+            Login
+          </nuxt-link>
+        </div>
+      </b-navbar-item>
+    </template>
+  </b-navbar>
+</template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,74 +1,28 @@
 <template>
   <div>
-    <nav
-      class="navbar header has-shadow is-primary"
-      role="navigation"
-      aria-label="main navigation"
-    >
-      <div class="navbar-brand">
-        <a
-          class="navbar-item"
-          href="/"
-        >
-          <img
-            src="~assets/buefy.png"
-            alt="Buefy"
-            height="28"
-          >
-        </a>
-
-        <div class="navbar-burger">
-          <span />
-          <span />
-          <span />
-        </div>
-      </div>
-    </nav>
-
-    <section class="main-content columns">
-      <aside class="column is-2 section">
-        <p class="menu-label is-hidden-touch">
-          General
+    <Navbar />
+    <main><nuxt /></main>
+    <footer class="footer">
+      <div class="content has-text-centered">
+        <p>
+          <strong>Staart</strong> by
+          <a href="https://anandchowdhary.com">Anand Chowdhary</a> from
+          <a href="https://o15y.com">O15Y</a>. The source code is licensed
+          <a href="http://opensource.org/licenses/mit-license.php">MIT</a>.
         </p>
-        <ul class="menu-list">
-          <li
-            v-for="(item, key) of items"
-            :key="key"
-          >
-            <nuxt-link
-              :to="item.to"
-              exact-active-class="is-active"
-            >
-              <b-icon :icon="item.icon" /> {{ item.title }}
-            </nuxt-link>
-          </li>
-        </ul>
-      </aside>
-
-      <div class="container column is-10">
-        <nuxt />
       </div>
-    </section>
+    </footer>
   </div>
 </template>
 
-<script>
-export default {
-  data () {
-    return {
-      items: [
-        {
-          title: 'Home',
-          icon: 'home',
-          to: { name: 'index' }
-        },
-        {
-          title: 'Inspire',
-          icon: 'lightbulb',
-          to: { name: 'inspire' }
-        }
-      ]
-    }
-  }
+<script lang="ts">
+import { Vue, Component } from "vue-class-component";
+import Navbar from "@/components/layout/Navbar.vue";
+
+@Component({
+  components: { Navbar }
+})
+export default class Navbar extends Vue {
+  message: string = "Hello!";
 }
 </script>
