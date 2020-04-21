a5d1d98cca39d5b369f47d26868a6af7437ba981
Anand Chowdhary <mail@anandchowdhary.com>
2020-04-22 03:33:56 +0530

    :recycle: Use shared auth layout

--- pages/auth/login.vue
+++ pages/auth/login.vue
@@ -1,42 +1,31 @@
 <template>
-  <div class="container">
-    <div class="columns is-centered">
-      <div class="column is-one-third">
-        <div class="card" style="margin: 7.5vw 0">
-          <div class="card-content">
-            <h1 class="title is-4">Login</h1>
-            <div class="content">
-              <form @submit.prevent="login">
-                <b-field label="Email">
-                  <b-input v-model="email" type="email" required />
-                </b-field>
-                <b-field label="Password">
-                  <b-input v-model="password" type="password" password-reveal />
-                </b-field>
-                <b-button
-                  type="is-primary"
-                  native-type="submit"
-                  :loading="loading"
-                  >Login to your account</b-button
-                >
-              </form>
-              <p style="margin-top: 1rem">
-                <a href="#">Forgot your password?</a>
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
+  <div>
+    <h1 class="title is-4">Login</h1>
+    <div class="content">
+      <form @submit.prevent="login">
+        <b-field label="Email">
+          <b-input v-model="email" type="email" required />
+        </b-field>
+        <b-field label="Password">
+          <b-input v-model="password" type="password" password-reveal />
+        </b-field>
+        <b-button type="is-primary" native-type="submit" :loading="loading"
+          >Login to your account</b-button
+        >
+      </form>
+      <p style="margin-top: 1rem">
+        <a href="#">Forgot your password?</a>
+      </p>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import Vue from "vue";
-import Component from "vue-class-component";
+import { Vue, Component } from "vue-property-decorator";
 
 @Component({
-  middleware: "unauthenticated"
+  middleware: "unauthenticated",
+  layout: "auth"
 })
 export default class Login extends Vue {
   email = "";
--- pages/auth/register.vue
+++ pages/auth/register.vue
@@ -1,58 +1,46 @@
 <template>
-  <div class="container">
-    <div class="columns is-centered">
-      <div class="column is-one-third">
-        <div class="card" style="margin: 7.5vw 0">
-          <div class="card-content">
-            <h1 class="title is-4">Register</h1>
-            <div class="content">
-              <form @submit.prevent="login">
-                <b-field label="Name">
-                  <b-input v-model="name" type="text" required />
-                </b-field>
-                <b-field label="Email">
-                  <b-input v-model="email" type="email" required />
-                </b-field>
-                <b-field v-show="!hasPasswordless" label="Password">
-                  <b-input v-model="password" type="password" password-reveal />
-                </b-field>
-                <b-field>
-                  <b-checkbox v-model="hasPasswordless">
-                    Use passwordless login
-                  </b-checkbox>
-                </b-field>
-                <b-field>
-                  <b-checkbox v-model="hasInviteCode">
-                    I have an invitation code
-                  </b-checkbox>
-                </b-field>
-                <b-field v-show="hasInviteCode" label="Invitation code">
-                  <b-input v-model="invitedByUser" type="text" />
-                </b-field>
-                <b-button
-                  type="is-primary"
-                  native-type="submit"
-                  :loading="loading"
-                >
-                  Create an account
-                </b-button>
-              </form>
-            </div>
-          </div>
-        </div>
-      </div>
+  <div>
+    <h1 class="title is-4">Register</h1>
+    <div class="content">
+      <form @submit.prevent="login">
+        <b-field label="Name">
+          <b-input v-model="name" type="text" required />
+        </b-field>
+        <b-field label="Email">
+          <b-input v-model="email" type="email" required />
+        </b-field>
+        <b-field v-show="!hasPasswordless" label="Password">
+          <b-input v-model="password" type="password" password-reveal />
+        </b-field>
+        <b-field>
+          <b-checkbox v-model="hasPasswordless">
+            Use passwordless login
+          </b-checkbox>
+        </b-field>
+        <b-field>
+          <b-checkbox v-model="hasInviteCode">
+            I have an invitation code
+          </b-checkbox>
+        </b-field>
+        <b-field v-show="hasInviteCode" label="Invitation code">
+          <b-input v-model="invitedByUser" type="text" />
+        </b-field>
+        <b-button type="is-primary" native-type="submit" :loading="loading">
+          Create an account
+        </b-button>
+      </form>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import Vue from "vue";
-import Component from "vue-class-component";
+import { Vue, Component } from "vue-property-decorator";
 
 @Component({
-  middleware: "unauthenticated"
+  middleware: "unauthenticated",
+  layout: "auth"
 })
-export default class Login extends Vue {
+export default class Register extends Vue {
   name = "";
   email = "";
   password = "";
