2db97c69458575f4232282ffd2037391160fc907
Anand Chowdhary <mail@anandchowdhary.com>
2020-11-15 13:12:08 +0530

    :lipstick: Add carbon navbar UI

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -1,60 +1,54 @@
-<script lang="ts">
-	export let segment: string;
+<script>
+  import {
+    Header,
+    HeaderAction,
+    HeaderActionSearch,
+    HeaderGlobalAction,
+    HeaderPanelDivider,
+    HeaderPanelLink,
+    HeaderPanelLinks,
+    HeaderUtilities,
+    SkipToContent,
+  } from "carbon-components-svelte";
+  import Moon20 from "carbon-icons-svelte/lib/Moon20";
+
+  let isSideNavOpen = false;
+  let isOpen = false;
+  let dark = undefined;
+  let href = "";
+
+  $: {
+    href = dark
+      ? "https://unpkg.com/carbon-components-svelte@0.22.0/css/g100.css"
+      : "https://unpkg.com/carbon-components-svelte@0.22.0/css/g10.css";
+  }
 </script>
 
 <style>
-	nav {
-		border-bottom: 1px solid rgba(255,62,0,0.1);
-		font-weight: 300;
-		padding: 0 1em;
-	}
-
-	ul {
-		margin: 0;
-		padding: 0;
-	}
-
-	/* clearfix */
-	ul::after {
-		content: '';
-		display: block;
-		clear: both;
-	}
-
-	li {
-		display: block;
-		float: left;
-	}
-
-	[aria-current] {
-		position: relative;
-		display: inline-block;
-	}
-
-	[aria-current]::after {
-		position: absolute;
-		content: '';
-		width: calc(100% - 1em);
-		height: 2px;
-		background-color: rgb(255,62,0);
-		display: block;
-		bottom: -1px;
-	}
-
-	a {
-		text-decoration: none;
-		padding: 1em 0.5em;
-		display: block;
-	}
+  :global(.OK .bx--toggle-input__label .bx--toggle__switch) {
+    margin-top: 0;
+  }
 </style>
 
-<nav>
-	<ul>
-		<li><a aria-current="{segment === undefined ? 'page' : undefined}" href=".">home</a></li>
-		<li><a aria-current="{segment === 'about' ? 'page' : undefined}" href="about">about</a></li>
-
-		<!-- for the blog link, we're using rel=prefetch so that Sapper prefetches
-		     the blog data when we hover over the link or tap it on a touchscreen -->
-		<li><a rel=prefetch aria-current="{segment === 'blog' ? 'page' : undefined}" href="blog">blog</a></li>
-	</ul>
-</nav>
+<svelte:head>
+  <link rel="stylesheet" {href} />
+</svelte:head>
+<Header company="Staart" platformName="UI" bind:isSideNavOpen>
+  <div slot="skip-to-content">
+    <SkipToContent />
+  </div>
+  <HeaderUtilities>
+    <HeaderActionSearch />
+    <HeaderGlobalAction
+      aria-label="Toggle dark mode"
+      icon={Moon20}
+      on:click={() => (dark = !dark)} />
+    <HeaderAction bind:isOpen>
+      <HeaderPanelLinks>
+        <HeaderPanelDivider>Admin</HeaderPanelDivider>
+        <HeaderPanelLink>Change password</HeaderPanelLink>
+        <HeaderPanelLink>Logout</HeaderPanelLink>
+      </HeaderPanelLinks>
+    </HeaderAction>
+  </HeaderUtilities>
+</Header>
