6e2dfd08d87b92dab0628df0829e14995165cc0c
Anand Chowdhary <mail@anandchowdhary.com>
2020-11-15 17:26:42 +0530

    :construction: Add sessions

---
+++ src/routes/users/[id]/sessions.svelte
@@ -0,0 +1,103 @@
+<script lang="ts">
+  import { stores } from "@sapper/app";
+  import {
+    Button,
+    DataTable,
+    DataTableSkeleton,
+    InlineLoading,
+    InlineNotification,
+    Modal,
+  } from "carbon-components-svelte";
+  import SubtractAlt20 from "carbon-icons-svelte/lib/SubtractAlt20";
+  import { onMount } from "svelte";
+  import { api } from "../../../helpers/api";
+
+  const { page } = stores();
+  const { id } = $page.params;
+
+  let sessions: any[] | null = null;
+  let loading = true;
+  let state = "ready";
+  let errorMessage = "";
+  let openDelete = false;
+  let deleteSession: number | null = null;
+  let currentSessionToken = null;
+  onMount(async () => {
+    sessions = await api("GET", `/users/${id}/sessions`);
+    loading = false;
+  });
+
+  const remove = async () => {
+    state = "removing";
+    openDelete = false;
+    try {
+      await api("DELETE", `/users/${id}/sessions/${deleteSession}`);
+      sessions = sessions.filter((i) => i.id !== deleteSession);
+      state = "removed";
+      setTimeout(() => (state = "ready"), 5000);
+    } catch (error) {
+      errorMessage = error.message;
+      state = "ready";
+    }
+    deleteSession = null;
+  };
+</script>
+
+<h1>Sessions</h1>
+
+{#if errorMessage}
+  <InlineNotification kind="error" title={errorMessage} />
+{/if}
+
+<Modal
+  bind:open={openDelete}
+  danger
+  modalHeading="Log out of session"
+  primaryButtonText="Log out"
+  secondaryButtonText="Cancel"
+  on:click:button--secondary={() => (openDelete = false)}
+  on:submit={() => remove()}>
+  <p>Are you sure you want to log out of this session?</p>
+</Modal>
+
+{#if loading}
+  <DataTableSkeleton showHeader={false} showToolbar={false} />
+{:else if sessions}
+  <DataTable
+    sortable
+    headers={[{ key: 'updatedAt', value: 'Last used' }, { key: 'browser', value: 'Device' }, { key: 'city', value: 'Location' }, { key: 'actions', value: '' }]}
+    rows={sessions}>
+    <span slot="cell-header" let:header>
+      {#if header.key === 'port'}
+        {header.value}
+        (network)
+      {:else}{header.value}{/if}
+    </span>
+    <span slot="cell" let:row let:cell>
+      {#if cell.key === 'updatedAt'}
+        {new Date(cell.value).toLocaleString()}
+      {:else if cell.key === 'actions'}
+        <div class="align-right">
+          {#if currentSessionToken !== row.token}
+            <Button
+              icon={SubtractAlt20}
+              on:click={() => {
+                openDelete = true;
+                deleteSession = row.id;
+              }}
+              hasIconOnly
+              kind="danger"
+              tooltipPosition="bottom"
+              tooltipAlignment="center"
+              iconDescription="Log out" />
+          {/if}
+        </div>
+      {:else}{cell.value}{/if}
+    </span>
+  </DataTable>
+  {#if state === 'removing'}
+    <InlineLoading status="active" description="Logging out..." />
+  {:else if state === 'removed'}
+    <InlineLoading status="finished" description="Logged out" />
+  {/if}
+{/if}
--- src/routes/users/_layout.svelte
+++ src/routes/users/_layout.svelte
@@ -33,16 +33,18 @@
     <SideNavLink text="Emails" href="/users/{id}/emails" />
     <SideNavLink text="Groups" href="/users/{id}/groups" />
     <SideNavMenu text="Security">
-      <SideNavMenuItem href="/" text="Password" />
-      <SideNavMenuItem href="/" text="Sessions" />
-      <SideNavMenuItem href="/" text="Multi-factor authentication" />
+      <SideNavMenuItem href="/users/{id}/password" text="Password" />
+      <SideNavMenuItem href="/users/{id}/sessions" text="Sessions" />
+      <SideNavMenuItem
+        href="/users/{id}/mfa"
+        text="Multi-factor authentication" />
     </SideNavMenu>
     <SideNavMenu text="Developers">
-      <SideNavMenuItem href="/" text="API keys" />
+      <SideNavMenuItem href="/users/{id}/api-keys" text="API keys" />
     </SideNavMenu>
     <SideNavMenu text="Advanced">
-      <SideNavMenuItem href="/" text="Merge account" />
-      <SideNavMenuItem href="/" text="Delete account" />
+      <SideNavMenuItem href="/users/{id}/merge" text="Merge account" />
+      <SideNavMenuItem href="/users/{id}/deactivate" text="Delete account" />
     </SideNavMenu>
   </SideNavItems>
 </SideNav>
